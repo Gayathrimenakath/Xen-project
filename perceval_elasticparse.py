0e2facf2af57109d41fc1b3948c9f7aa0d612c2b
Gayathrimenakath <gayathrimenakath@gmail.com>
2017-03-25 16:28:27 +0530

    Threaded data uploaded to Elasticsearch index

--- perceval_elasticparse.py
+++ perceval_elasticparse.py
@@ -1,9 +1,8 @@
-from perceval.backends.core.mbox import MBox
-import elasticsearch
-
+#!/usr/bin/env python3
 import logging
-
 import argparse
+import json
+import elasticsearch
 
 msg_ids = []
 msg_json = []
@@ -14,42 +13,41 @@ es = elasticsearch.Elasticsearch(['http://localhost:9200/'])
 
 
 # Create a mbox object, pointing to uri, using dir_path for fetching
-class MboxParser:
+class MboxElastic:
 
-    def getmbox(self, mbox_files):
-        mbox_parser = MBox(
-                uri = mbox_files,
-                dirpath='./mboxes'
-        )
-        return mbox_parser.fetch()
-
-    def elastic(self, mbox_files, indexname):
+    def elastic(self, threaded_files, indexname):
         try:
             # Create the 'mboxes' index in ElasticSearch
             es.indices.create(indexname)
         except elasticsearch.exceptions.RequestError:
             print('Index already exisits, remove it before running this script again.')
             exit()
-        percevalout = self.getmbox(mbox_files)
-        message_id = ''
-        # Fetch all mboxes as an iteratoir, and iterate it uploading to ElasticSearch
-        for mboxes in percevalout:           
-            # Create the object (dictionary) to upload to ElasticSearch
-            summary = {'message': mboxes['data']['Message-ID'],
-                       'Sender': mboxes['data']['X-Env-Sender'],
-                       'subject': mboxes['data']['Subject']}
-            print(summary)
-            # Upload the object to ElasticSearch
-            es.index(index=indexname, doc_type='summary', body=summary)
+        
+        jfile = None
+        with open(threaded_files) as f:
+            for line in f:
+                while True:
+                    try:
+                        jfile = json.loads(line, strict=False)
+                        break
+                    except ValueError:
+                        # Not yet a complete JSON value
+                        line += next(f)
+
+                
+                es.index(index=indexname, doc_type='summary', body=jfile)
+            f.close()
+                
+                
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mbox",required=True,help="Give the name of the mbox file to be parsed")
+    parser.add_argument("--filename",required=True,help="Give the name of the threaded file")
     parser.add_argument("--indexname", required=True, help="Name of the Elasticsearch index")
     args = parser.parse_args()
     logging.basicConfig(filename='perceval_mbox_parse.log', level=logging.DEBUG)
-    mparser = MboxParser()
-    mparser.elastic(args.mbox,args.indexname)
+    mparser = MboxElastic()
+    mparser.elastic(args.filename,args.indexname)
 
 if __name__ == "__main__":
     main()
